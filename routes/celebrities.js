7ad3d4f1525221c96701ffc6988a506ab55313e2
Esteban <esteban.github@gmail.com>
2021-09-06 17:55:12 +0200

    add iteration 3

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -6,9 +6,21 @@ router.get('/', (req, res, next) => {
 	Celebrity.find()
 		.then(celebritiesFromDB => {
 			//console.log(celebritiesFromDB);
-			res.render('celebrities', { celebritiesList: celebritiesFromDB });
+			res.render('celebrities/index', { celebritiesList: celebritiesFromDB });
 		})
 		.catch(err => {
+			next(err);
+		})
+})
+
+router.get('/:id', (req, res, next) => {
+    //console.log(req.params);
+    Celebrity.findById(req.params.id)
+        .then(celebrityFromDB => {
+            //console.log(celebritiesFromDB)
+            res.render('celebrities/show', { celebrityCard: celebrityFromDB })
+        })
+        .catch(err => {
 			next(err);
 		})
 })
